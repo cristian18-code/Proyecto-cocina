2d07901e64c55a05fe47ce70099fec697739e9eb
cristian18-code <cristiangarzon180@gmail.com>
2020-06-27 16:42:39 -0500

    Christian

---
+++ logica/conexion.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pw = "";
+    $db = "cocina";
+
+    $conectar = mysqli_connect($host, $user, $pw, $db);
+?>
--- tabla_productos.php
+++ tabla_productos.php
@@ -1,3 +1,9 @@
+<?php
+	include 'logica/conexion.php';
+	$consulta_tipoproducto = "SELECT * FROM tipoproducto";
+	$ejecutar = mysqli_query($conectar, $consulta_tipoproducto);
+	mysqli_close($conectar);
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -77,11 +83,11 @@ $(document).ready(function(){
 			<div class="table-title">
 				<div class="row">
 					<div class="col-sm-6">
-						<h2>Manage <b>Employees</b></h2>
+						<h2>Tipos de <b>Productos</b></h2>
 					</div>
 					<div class="col-sm-6">
-						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
-						<a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
+						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Agregar Tipo</span></a>
+						<a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Eliminar</span></a>						
 					</div>
 				</div>
 			</div>
@@ -94,11 +100,10 @@ $(document).ready(function(){
 								<label for="selectAll"></label>
 							</span>
 						</th>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Address</th>
-						<th>Phone</th>
-						<th>Actions</th>
+						<th>Codigo</th>
+						<th>Nombre</th>
+						<th>¿Refrigerable?</th>
+						<th>¿Tiene vencimiento?</th>
 					</tr>
 				</thead>
 				<tbody>
