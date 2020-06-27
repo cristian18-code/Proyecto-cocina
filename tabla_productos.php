eeb5a42388c9077d4a6b2fcf6460a22212472ab6
cristian18-code <cristiangarzon180@gmail.com>
2020-06-27 17:23:32 -0500

    Cambiado formulario de tipo de producto

--- tabla_productos.php
+++ tabla_productos.php
@@ -143,32 +143,27 @@ $(document).ready(function(){
 <div id="addEmployeeModal" class="modal fade">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			<form>
+			<form action="grd_tipoproducto" method="post">
 				<div class="modal-header">						
-					<h4 class="modal-title">Add Employee</h4>
+					<h4 class="modal-title">Agregar Tipo de producto</h4>
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 				</div>
 				<div class="modal-body">					
 					<div class="form-group">
-						<label>Name</label>
-						<input type="text" class="form-control" required>
+						<label>Nombre</label>
+						<input type="text" name="nombre" class="form-control" required>
 					</div>
 					<div class="form-group">
-						<label>Email</label>
-						<input type="email" class="form-control" required>
+						<label>¿Es refrigerable?</label>
+						<input type="email" name="refrigerable" class="form-control" required>
 					</div>
 					<div class="form-group">
-						<label>Address</label>
-						<textarea class="form-control" required></textarea>
+						<label>¿Tiene vencimiento?</label>
+						<input class="form-control" name="vencimiento" required>
 					</div>
-					<div class="form-group">
-						<label>Phone</label>
-						<input type="text" class="form-control" required>
-					</div>					
-				</div>
 				<div class="modal-footer">
 					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-					<input type="submit" class="btn btn-success" value="Add">
+					<input type="submit" class="btn btn-success" value="Añadir">
 				</div>
 			</form>
 		</div>
