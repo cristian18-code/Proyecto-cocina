ca374a2acf40466de0afa5629e55bafc9bddeade
cristian18-code <cristiangarzon180@gmail.com>
2020-06-29 14:49:16 -0500

    Se quitaron botones sin funcionar

--- tabla_administrador.php
+++ tabla_administrador.php
@@ -55,31 +55,30 @@ $(document).ready(function(){
 <body>
 
 <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="index.html">COCINA</a>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="tabla_despensa.php">Despensas</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="tabla_Productos.php">Productos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="tabla_distribuidor.php">Distribuidores</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-          </form>
-        </div>
-</nav>   
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="index.html">COCINA</a>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tabla_despensa.php">Despensas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tabla_Productos.php">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tabla_distribuidor.php">Distribuidores</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tabla_administrador.php">Administradores</a>
+          </li>
+        </ul>        
+      </div>
+</nav>
 
 <br> <br><br> <br><br> 
 
